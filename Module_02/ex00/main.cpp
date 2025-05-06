4bc65677f561c581c6d68b98f2cf9d1c1e8c435b
lnicolosi <l.nicolosi8@gmail.com>
2025-05-06 15:58:49 +0200

    module2 start

---
+++ Module_02/ex00/main.cpp
@@ -0,0 +1,16 @@
+// #ifndef MYCLASS_HPP
+// #define MYCLASS_HPP
+
+// class MyClass
+// {
+// public:
+//     MyClass();                            // Constructeur par défaut
+//     MyClass(const MyClass& other);         // Constructeur de copie
+//     MyClass& operator=(const MyClass& other); // Opérateur d'affectation
+//     ~MyClass();                            // Destructeur
+
+// private:
+//     int _value; // Attribut privé exemple
+// };
+
+// #endif // MYCLASS_HPP
